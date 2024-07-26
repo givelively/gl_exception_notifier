16050fb8af43b35f720256731f526ce1d95fcd96
Seth Herr <seth.william.herr@gmail.com>
2024-07-26 10:32:22 -0700

    Add note about updating the rules_version

--- spec/rubocop_rules_spec.rb
+++ spec/rubocop_rules_spec.rb
@@ -3,7 +3,9 @@
 require 'spec_helper'
 
 RSpec.describe '.rubocop_rules.yml' do # rubocop:disable RSpec/DescribeClass
-  it 'has the correct rules' do
+  rules_version = '3.2.3' # Update this when the rules are written with a new Ruby version
+
+  it "has the correct rules for Ruby: #{rules_version}", skip: rules_version != RUBY_VERSION do
     # If this spec failed, it's because Rubocop rules changed and you need to run:
     # bin/lint --write-rubocop-rules
     # (and commit the updated .rubocop_rules.yml)
